f20ec52df74eb57efba311884ae142b694143be0
Arturzek <42453634+Arturzek@users.noreply.github.com>
2018-09-15 12:43:07 -0300

    To block all inputs during the

---
+++ Classes/blockinput.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+// Copied from: https://stackoverflow.com/questions/20841501/blockinput-method-doesnt-work-on-windows-7
+
+namespace blockinput
+{
+    static class InputBlocker
+    {
+        [DllImport("user32.dll")]
+        static extern bool BlockInput(bool fBlockIt);
+        private static Timer timer = new Timer();
+        static InputBlocker()
+        {
+            timer.Tick += new EventHandler(tick);
+        }
+        public static void Block(int mill)
+        {
+            BlockInput(true);
+            timer.Interval = mill;
+            timer.Start();
+        }
+        private static void tick(object sender, EventArgs e)
+        {
+            BlockInput(false);
+            timer.Stop();
+        }
+    }
+}
